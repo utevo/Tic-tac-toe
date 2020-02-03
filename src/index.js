b8bc96f72af0c6c8fbfbee824fc1809b2a78d99d
Michał Kowieski <kowieski.m98@gmail.com>
2020-02-03 22:17:22 +0100

    Refactoring

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square" onClick={function() { alert('wow'); }}>
+        <button className="square" onClick={() => { alert('wow'); }}>
           {this.props.value}
         </button>
       );
