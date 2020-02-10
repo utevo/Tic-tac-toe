038bfc7dc0b32ada7d9bd02b175e0f6c285ae7a3
Michał Kowieski <kowieski.m98@gmail.com>
2020-02-10 21:25:01 +0100

    Remove a Square class constructor

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 
 
 class Square extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-
     render() {
       return (
         <button className="square"
