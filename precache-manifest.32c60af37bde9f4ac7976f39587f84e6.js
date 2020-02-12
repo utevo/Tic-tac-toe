d5cdfc7448ef574f80ba3b6d17ddaa5153174a0f
Michał Kowieski <kowieski.m98@gmail.com>
2020-02-12 20:28:14 +0100

    Updates

---
+++ precache-manifest.32c60af37bde9f4ac7976f39587f84e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14766341e34afa5bc6a9ea9c856b5fb9",
+    "url": "/Tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d1f8712a6610f6ede9db",
+    "url": "/Tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "7a52f19f23e616b1a6b0",
+    "url": "/Tic-tac-toe/static/js/2.4ea46d4b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Tic-tac-toe/static/js/2.4ea46d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f8712a6610f6ede9db",
+    "url": "/Tic-tac-toe/static/js/main.d7213468.chunk.js"
+  },
+  {
+    "revision": "6cec87fdb636719e56db",
+    "url": "/Tic-tac-toe/static/js/runtime-main.38ccf179.js"
+  }
+]);
